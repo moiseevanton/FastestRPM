a65e348dc3647b0d27e834d08cd29a810cdba7bf
moiseevanton <athidif94@gmail.com>
2016-05-12 22:03:05 -0400

    Final but incomplete code

--- FastestRPM/ViewController.h
+++ FastestRPM/ViewController.h
@@ -10,6 +10,16 @@
 
 @interface ViewController : UIViewController
 
+#define RADIANS(degrees) (((degrees) * M_PI) / 180.0)
+#define MIN_DEGREES -135.0
+#define MAX_DEGREES 135.0
+#define RANGE_DEGREES (MAX_DEGREES - MIN_DEGREES)
+
+#define LIMIT_VELOCITY 7500.0
+#define LIMIT_VELOCITY_DELTA 10.0
+
+#define RESET_DELAY 0.1
+#define VELOCITY_DELAY 0.1
 
 @end
 
--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -10,6 +10,16 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UIImageView *speedometer;
+
+@property (strong, nonatomic) UIImageView *needle;
+
+@property (nonatomic) CGFloat currVelocity;
+
+@property (nonatomic) CGFloat maxVelocity;
+
+- (void)moveNeedleWithVelocity:(CGFloat)velocity;
+
 @end
 
 @implementation ViewController
@@ -17,11 +27,71 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // seting up the speedometer
+    self.speedometer = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"speedometer"]];
+    self.speedometer.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.speedometer];
+
+    [self.speedometer.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [self.speedometer.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
+    
+    // setting up the needle
+    self.needle = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"needle"]];
+    self.needle.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.speedometer addSubview:self.needle];
+    
+    [self.needle.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [self.needle.centerYAnchor constraintEqualToAnchor:self.speedometer.centerYAnchor].active = YES;
+    [self.needle.widthAnchor constraintEqualToConstant:250].active = YES;
+    [self.needle.heightAnchor constraintEqualToConstant:250].active = YES;
+    
+    self.needle.transform = CGAffineTransformMakeRotation(2.5);
+    
+    UIPanGestureRecognizer *circularMotions = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(makeCircularMotions:)];
+    
+    [self.view setUserInteractionEnabled:YES];
+    [self.view addGestureRecognizer:circularMotions];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)makeCircularMotions:(UIPanGestureRecognizer *) sender {
+    
+    CGPoint componentVelocity = [sender velocityInView:self.view];
+    CGFloat velocity = sqrt(pow(componentVelocity.x, 2) + pow(componentVelocity.y, 2));
+    [self moveNeedleWithVelocity:velocity];
+    
+    if ((sender.state == UIGestureRecognizerStateEnded) || (sender.state == UIGestureRecognizerStateCancelled) || (sender.state == UIGestureRecognizerStateFailed)) {
+        
+        [UIView animateWithDuration:2 animations:^{
+            self.needle.transform = CGAffineTransformMakeRotation(-3.9);
+            
+        }completion:^(BOOL finished) {
+            nil;
+        }];
+        
+    }
+    
+    
+}
+
+- (void)moveNeedleWithVelocity:(CGFloat)velocity {
+    
+    self.currVelocity = velocity;
+    
+    self.maxVelocity = MAX(self.maxVelocity, velocity);
+    
+    CGFloat velocityProportion = velocity / LIMIT_VELOCITY;
+    
+    CGFloat degrees = MIN(RANGE_DEGREES * velocityProportion, RANGE_DEGREES);
+    
+    self.needle.transform = CGAffineTransformMakeRotation(RADIANS(degrees));
+    
 }
 
 @end
